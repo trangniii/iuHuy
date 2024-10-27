ab63a8b4398fd7de825481a11f14697505ae6e0d
unknown <>
2024-10-27 21:12:25 +0700

    fix: small fix bug

--- utils/utils.js
+++ utils/utils.js
@@ -1,14 +1,18 @@
-import { endOfWeek, startOfWeek } from "date-fns";
-
-export function getOffsetLimit(page, pageSize) {
+const { startOfWeek, endOfWeek } = require("date-fns");
+function getOffsetLimit(page, pageSize) {
   return {
     offset: (page - 1) * pageSize,
     limit: pageSize,
   };
 }
 
-export function getWeekStartEndDates() {
+function getWeekStartEndDates() {
   const startDate = startOfWeek(new Date(), { weekStartsOn: 1 });
   const endDate = endOfWeek(new Date(), { weekStartsOn: 1 });
   return { startDate, endDate };
 }
+
+module.exports = {
+  getOffsetLimit,
+  getWeekStartEndDates,
+};
